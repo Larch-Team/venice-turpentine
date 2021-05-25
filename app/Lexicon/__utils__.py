0baca63ea3a4257ba0bfca95a3fefb8c7b3e2b31
Jakub Dakowski <jakubdakowski@gmail.com>
2021-05-25 03:29:36 +0200

    continuing parser implementation

--- app/Lexicon/__utils__.py
+++ app/Lexicon/__utils__.py
@@ -4,16 +4,4 @@ from sentence import Sentence
 lexicon = None
 
 
-class CompilerError(Exception):
-    pass
-
-
-class MultipleTypesError(CompilerError):
-
-    def __init__(self, multiple, *args, **kwargs):
-        lists = [" - ".join((i[0], ", ".join(i[1]))) for i in multiple.items()]
-        msg = "\n".join(("Multiple types found for:", *lists))
-        super().__init__(msg, *args, **kwargs)
-
-
 NON_CONVERTIBLE = ("(", ")")
--- app/parser.py
+++ app/parser.py
@@ -1,16 +1,16 @@
 from typing import Any, Iterable, Union
 from dataclasses import dataclass
 from abc import ABC, abstractmethod
+from collections import namedtuple
 import sly
 
-@dataclass
-class Rule(object):
-    context: dict[str, Any]
-    lexem: str
-    toktype: str
+LITERALS = {'(', ')'}
 
-# def dictSortedValues(d: dict[str, Any]):
-#     return [i for _,i in sorted(d.items(), key=lambda x: x[0])]
+LexerRule = namedtuple('LexerRule', ('constraints', 'token', 'lexems'))
+
+class LrchLexerError(Exception):
+    pass
+
 
 class Lexicon(object):
     STACK = []
@@ -18,36 +18,79 @@ class Lexicon(object):
     def __init__(self) -> None:
         super().__init__()
         self.rules = set()
+        self.is_lowercase = False
 
-    def __setattr__(self, token: str, lexems: Union[str, Iterable[str]]) -> None:
+    def __setitem__(self, token: str, lexems: Union[str, Iterable[str]]) -> None:
         if isinstance(lexems, str):
-            self.rules.add(Rule(self.STACK.copy(), lexems, token))
+            self.rules.add(LexerRule(self.STACK.copy(), token, [lexems]))
+            self.needs_casing |= lexems.isupper()
         elif isinstance(Iterable):
-            for i in lexems:
-                self.__setattr__(token, i) 
+            self.rules.add(LexerRule(self.STACK.copy(), token, lexems))
+            self.needs_casing |= any((i.isupper() for i in lexems))
 
 
-    def _lexicon_build(self, **kwargs: dict[str, Any]):
-        used = {}
-        
-        # skompiluj listę used na podstawie kwargs i constraints danych reguł
+class BuiltLexer(object):
+
+    def __init__(self, lex: Lexicon, **kwargs: dict[str, Any]) -> None:
+        super().__init__()
+        self.needs_casing = lex.needs_casing
 
         class _Lex(sly.Lexer):
-            tokens = set(used.keys())
+            literals = LITERALS
+            tokens = set()
 
-        # Dodać do lexiconu rzeczy z used
+            def error(self, t):
+                raise LrchLexerError(f'{t} is not tokenizable')
+
+        used = self._join_rules(self._filter_constraints(lex, kwargs))
+        used = ((key, r"|".join((f"({i})" for i in val)))
+                for key, val in used.items())
+        for token, lexems in sorted(used, key=lambda x: len(x[1]), reverse=True):
+            setattr(_Lex, token, lexems)
+            _Lex.tokens.add(token)
+
+        self.lexer = _Lex()
+
+    @staticmethod
+    def _filter_constraints(lex: Lexicon, const: dict[str, Any]) -> Iterable[tuple[str, tuple[str]]]:
+        for constraints, token, lexems in lex.rules:
+            if all((i in const.items() for i in constraints if i[0] != 'findnew')):
+                yield token, lexems
+
+    @staticmethod
+    def _join_rules(rules: Iterable[tuple[str, tuple[str]]]) -> dict[str, tuple[str]]:
+        d = {}
+        for token, lexems in rules:
+            if token in d:
+                d[token].extend(lexems)
+            else:
+                d[token] = lexems
+        return d
+
+    def tokenize(self, formula: str):
+        if not self.needs_casing:
+            formula = formula.lower()
+        
+        sentence = []
+        for i in self.lexer.tokenize(formula):
+            if i.value in LITERALS:
+                sentence.append(i.value)
+            else:
+                sentence.append(f"{}")
+
 
 class RuleConstraint(ABC):
 
     @abstractmethod
     def __init__(self, tag: Any) -> None:
         super().__init__()
-        self.tag = tag
+        self.tag=tag
 
 
     def __enter__(self) -> None:
         Lexicon.STACK.append((type(self).__name__, self.tag))
         return
+
 
     def __exit__(self) -> None:
         Lexicon.STACK.pop()
