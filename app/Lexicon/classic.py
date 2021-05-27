95b07ee1262273aeee87000edc2f06963cba7e9b
Jakub Dakowski <jakubdakowski@gmail.com>
2021-05-28 00:24:00 +0200

    change import to from _ import *

--- app/Lexicon/classic.py
+++ app/Lexicon/classic.py
@@ -1,38 +1,38 @@
 """
 Transkrypcja basic.py na nowy format definicji języka
 """
-import Lexicon.__utils__ as utils
+from Lexicon.__utils__ import *
 
 SOCKET = 'Lexicon'
 VERSION = '0.0.1'
 
-Lex = utils.Lexicon()
+Lex = Lexicon()
 
-with utils.use_language('propositional'):
-    with utils.use_language('uses negation'):
+with use_language('propositional'):
+    with use_language('uses negation'):
         Lex['not'] = 'not', '~', r'\!'
     Lex['and'] = 'oraz', 'and', r'\^', '&'
     Lex['or'] = 'lub', 'or', r'\|', 'v'
     Lex['imp'] = 'imp', '->'
-    with utils.find_new():
+    with find_new():
         Lex['sentvar'] = r'[a-z]'
-    with utils.no_generation():
+    with no_generation():
         Lex['sentvar'] = r'\w+'
 
-with utils.use_language('predicate'):
+with use_language('predicate'):
     Lex['forall'] = 'forall', '/\\', 'A'
     Lex['exists'] = 'exists', '\\/', 'E'
-    with utils.find_new():
+    with find_new():
         Lex['constant'] = r'[a-t]', r'\d'
         Lex['indvar'] = r'[u-z]'
         Lex['predicate'] = r'[P-Z]'
         Lex['function'] = r'[F-O]'
 
-with utils.use_language('sequent calculus'):
+with use_language('sequent calculus'):
     Lex['turnstile'] = '=>', '|-'
     Lex['sep'] = ';'
     Lex['falsum'] = 'bot', 'F'
 
 
-def get_lexicon() -> utils.Lexicon:
+def get_lexicon() -> Lexicon:
     return Lex
