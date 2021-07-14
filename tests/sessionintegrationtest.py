85f2d06ba6b51f1a7e16e6ae119740514017d81b
ghostbuster265 <ghostbuster2651@gmail.com>
2021-07-15 01:17:01 +0200

    generation of command lists for integration tests

--- tests/sessionintegrationtest.py
+++ tests/sessionintegrationtest.py
@@ -8,19 +8,34 @@ from UserInterface import CLI as c
 runner = c.Runner()
 rules = runner.session.getrules()
 commands = list(c.command_dict.keys())
+for x in ['prove','plugin gen','write','exit']:
+        commands.remove(x)
 args = {'jump': [' left',' right',' <',' >'],
         'write': ['file'],
         'use': ['true and', 'false and', 'false or', 'true or', 'false imp', 'true imp', 'double not'],
         'plugin switch': ['UserInterface','Lexicon','Auto','FormalSystem','Output'], #jeszcze name
         'plugin list': ['UserInterface','Lexicon','Auto','FormalSystem','Output']}
 
-lists_acc = [[random.choice(commands) for _ in range(20)] for _ in range(50)]
+lists_acc = []
 lists_wrong = []
+for _ in range(50):
+        com_list = []
+        for _ in range(19):
+                com = random.choice(commands)
+                if com in args.keys():
+                        com_conf = [com,random.choice(args[com])]
+                        if com == 'plugin switch':
+                                com_conf = ' '.join([com_conf[0],com_conf[1],random.choice(runner('plugin list '+com_conf[-1]).split(sep='\n')[1].split(sep='; '))])
+                        else:
+                                com_conf = ' '.join(com_conf)
+                else:
+                        com_list.append(com_conf)
+        com_list.append('exit')
+        lists_acc.append(com_list)
+ 
 
-# for key in commands.keys(): print(key)
-# print(rules.keys())
-# print(lists[1])
+for list in lists_acc: print(list)
 
-for com_list in lists:
-    for com in com_list:
-        runner(com)
+# p = runner('plugin list FormalSystem')
+# p = p.split(sep='\n')[1].split(sep = '; ')
+# print(p)
