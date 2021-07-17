f6eac00f9e690bf8aa3fdf248734950cfacb54bd
ghostbuster265 <ghostbuster2651@gmail.com>
2021-07-17 16:30:22 +0200

    integration tests

---
+++ app/Lexicon/classic.py
@@ -0,0 +1,38 @@
+"""
+Transkrypcja basic.py na nowy format definicji języka
+"""
+from Lexicon.__utils__ import *
+
+SOCKET = 'Lexicon'
+VERSION = '0.0.1'
+
+Lex = Lexicon()
+
+with use_language('propositional'):
+    with use_language('uses negation'):
+        Lex['not'] = 'not', '~', r'\!'
+    Lex['and'] = 'oraz', 'and', r'\^', '&'
+    Lex['or'] = 'lub', 'or', r'\|', 'v'
+    Lex['imp'] = 'imp', '->'
+    with find_new():
+        Lex['sentvar'] = r'[a-z]'
+    with no_generation():
+        Lex['sentvar'] = r'\w+'
+
+with use_language('predicate'):
+    Lex['forall'] = 'forall', '/\\', 'A'
+    Lex['exists'] = 'exists', '\\/', 'E'
+    with find_new():
+        Lex['constant'] = r'[a-t]', r'\d'
+        Lex['indvar'] = r'[u-z]'
+        Lex['predicate'] = r'[P-Z]'
+        Lex['function'] = r'[F-O]'
+
+with use_language('sequent calculus'):
+    Lex['turnstile'] = '=>', '|-'
+    Lex['sep'] = ';'
+    Lex['falsum'] = 'bot', 'F'
+
+
+def get_lexicon() -> Lexicon:
+    return Lex
--- tests/sessionintegrationtest.py
+++ tests/sessionintegrationtest.py
@@ -4,38 +4,56 @@ import random
 sys.path.append('../app')
 from UserInterface import CLI as c
 
-#jeszcze tu wroce to takie prototypowe zabawy
+#jeszcze tu wroce
 runner = c.Runner()
 rules = runner.session.getrules()
 commands = list(c.command_dict.keys())
 for x in ['prove','plugin gen','write','exit']:
         commands.remove(x)
 args = {'jump': [' left',' right',' <',' >'],
-        'write': ['file'],
         'use': ['true and', 'false and', 'false or', 'true or', 'false imp', 'true imp', 'double not'],
-        'plugin switch': ['UserInterface','Lexicon','Auto','FormalSystem','Output'], #jeszcze name
+        'plugin switch': ['UserInterface','Lexicon','Auto','FormalSystem','Output'],
         'plugin list': ['UserInterface','Lexicon','Auto','FormalSystem','Output']}
 
 lists_acc = []
 lists_wrong = []
 for _ in range(50):
-        com_list = []
-        for _ in range(19):
-                com = random.choice(commands)
-                if com in args.keys():
-                        com_conf = [com,random.choice(args[com])]
-                        if com == 'plugin switch':
-                                com_conf = ' '.join([com_conf[0],com_conf[1],random.choice(runner('plugin list '+com_conf[-1]).split(sep='\n')[1].split(sep='; '))])
+        com_list_a = ['prove (p -> q) ^ p -> q']
+        com_list_w = ['prove (p -> q) ^ p -> q']
+        for _ in range(18):
+                com_a = random.choice(commands)
+                com_w = random.choice(commands)
+                add_arg = random.randint(0,9)
+                if add_arg%2:
+                        com_list_w.append(' '.join([com_w,random.choice(list(args.values())[random.randint(0,len(args.values())-1)])]))
+                else:
+                        com_list_w.append(com_w)
+                if com_a in args.keys():
+                        com_conf = [com_a,random.choice(args[com_a])]
+                        if com_a == 'plugin switch':
+                                com_conf = ' '.join([*com_conf,random.choice(runner('plugin list '+com_conf[-1]).split(sep='\n')[1].split(sep='; '))])
                         else:
                                 com_conf = ' '.join(com_conf)
+                        com_list_a.append(com_conf)
                 else:
-                        com_list.append(com_conf)
-        com_list.append('exit')
-        lists_acc.append(com_list)
+                        com_list_a.append(com_a)
+        com_list_a.append('exit')
+        com_list_w.append('exit')
+        lists_acc.append(com_list_a)
+        lists_wrong.append(com_list_w)
  
+print('POPRAWNE ARGUMENTY\n\n')
+for i,list in enumerate(lists_acc):
+        print(f'LISTA {i}\n')
+        for com in list:
+                print(f'{com}: --[{runner(com)}]--')
+print('\n\n ZLE ARGUMENTY\n\n')
+for i,list in enumerate(lists_wrong):
+        print(f'LISTA {i}\n')
+        for com in list:
+                print(f'{com}: --[{runner(com)}]--') 
 
-for list in lists_acc: print(list)
-
+# print(random.choice(list(args.values())[random.randint(0,len(args.values())-1)]))
 # p = runner('plugin list FormalSystem')
 # p = p.split(sep='\n')[1].split(sep = '; ')
 # print(p)
