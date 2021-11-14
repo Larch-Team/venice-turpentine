600c96148981b4ad7c3622e90f59afa4590f524c
Jakub Dakowski <jakubdakowski@gmail.com>
2021-11-14 22:09:40 +0100

    sent_to_plugins.py also copies __init__.py, __template__.py and __utils__.py

--- distributing/send_to_plugins.py
+++ distributing/send_to_plugins.py
@@ -13,5 +13,10 @@ to_submit = list(setups.values()) + sum(sum((list(i.values()) for i in plugins.v
 for i in to_submit:
     shutil.copyfile('app/appdata/'+i, '../larch-plugins/'+i)
     
+for i in plugins:
+    for j in ['__init__.py', '__template__.py', '__utils__.py']:
+        if os.path.isfile(f'app/appdata/plugins/{i}/{j}'):
+            shutil.copyfile(f'app/appdata/plugins/{i}/{j}', f'../larch-plugins/plugins/{i}/{j}')
+
 with open('../larch-plugins/files.json', 'w') as f:
     dump(files, f)
